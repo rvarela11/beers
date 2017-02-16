11e998c1704f7f3e0c2859000653961b292d4106
Robert Varela <rvarela@utexas.edu>
2017-02-15 22:15:32 -0600

    first draft

---
+++ scripts/js/app.js
@@ -0,0 +1,25 @@
+var app = angular.module('webapp', ['ngMaterial', 'ngMessages', 'ngResource']);
+
+  app.controller('DemoCtrl', ['$scope', '$http', function($scope, $http, $resource) {
+
+
+    $scope.view = {};
+      // use $http or $resource
+      // https://api.punkapi.com/v2/beers
+
+      $http({
+        method: 'GET',
+        url: 'https://api.punkapi.com/v2/beers'
+      }).then(function successCallback(response) {
+          $scope.view.results = response.data;
+        }, function errorCallback(response) {
+          console.log("Error",response);
+        });
+  }
+  ]);
+
+app.config(function($mdThemingProvider) {
+    $mdThemingProvider.theme('docs-dark', 'default')
+        .primaryPalette('yellow')
+        .dark();
+});
