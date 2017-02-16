b35fbaa9d57530c1194c0d37a122b1592876d6cd
Robert Varela <rvarela@utexas.edu>
2017-02-15 22:22:57 -0600

    cleaned up code

--- scripts/js/app.js
+++ scripts/js/app.js
@@ -2,19 +2,18 @@ var app = angular.module('webapp', ['ngMaterial', 'ngMessages', 'ngResource']);
 
   app.controller('DemoCtrl', ['$scope', '$http', function($scope, $http, $resource) {
 
-
     $scope.view = {};
       // use $http or $resource
       // https://api.punkapi.com/v2/beers
 
-      $http({
-        method: 'GET',
-        url: 'https://api.punkapi.com/v2/beers'
-      }).then(function successCallback(response) {
-          $scope.view.results = response.data;
-        }, function errorCallback(response) {
-          console.log("Error",response);
-        });
+    $http({
+      method: 'GET',
+      url: 'https://api.punkapi.com/v2/beers'
+    }).then(function successCallback(response) {
+        $scope.view.results = response.data;
+      }, function errorCallback(response) {
+        console.log("Error",response);
+    });
   }
   ]);
 
